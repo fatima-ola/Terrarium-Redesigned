a099ccedbd0ffe63f4356a177d75bdd0b0c9319e
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-06-03 23:40:36 +0100

    edited the typo error

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-dragElement(documnet.getElementById("plant1"));
-dragElement(documnet.getElementById("plant2"));
-dragElement(documnet.getElementById("plant3"));
-dragElement(documnet.getElementById("plant4"));
-dragElement(documnet.getElementById("plant5"));
-dragElement(documnet.getElementById("plant6"));
-dragElement(documnet.getElementById("plant7"));
-dragElement(documnet.getElementById("plant8"));
-dragElement(documnet.getElementById("plant9"));
-dragElement(documnet.getElementById("plant10"));
-dragElement(documnet.getElementById("plant11"));
-dragElement(documnet.getElementById("plant12"));
-dragElement(documnet.getElementById("plant13"));
-dragElement(documnet.getElementById("plant14"));
+dragElement(document.getElementById("plant1"));
+dragElement(document.getElementById("plant2"));
+dragElement(document.getElementById("plant3"));
+dragElement(document.getElementById("plant4"));
+dragElement(document.getElementById("plant5"));
+dragElement(document.getElementById("plant6"));
+dragElement(document.getElementById("plant7"));
+dragElement(document.getElementById("plant8"));
+dragElement(document.getElementById("plant9"));
+dragElement(document.getElementById("plant10"));
+dragElement(document.getElementById("plant11"));
+dragElement(document.getElementById("plant12"));
+dragElement(document.getElementById("plant13"));
+dragElement(document.getElementById("plant14"));
 
 function dragElement(terrariumElement){
     let pos1 = 0;
     pos2 = 0;
     pos3 = 0;
     pos4 = 0;
-    terrariumElement.onpointerdown = pointerDrag;
+    terrariumElement.onclick = pointerDrag;
+
     function pointerDrag(e) {
         e.preventDefault();
         console.log(e);
@@ -27,4 +28,20 @@ function dragElement(terrariumElement){
         document.onpointermove = elementDrag;
         document.onpointerup = stopElementDrag;
     }
+
+    function elementDrag(e){
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e .clientY;
+        console.log(pos1, pos2, pos3, pos4);
+        terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
+        terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+    }
+
+    function stopElementDrag() {
+        document.onpointerup = null;
+        document.onpointermove = null;
+    }
+    
 }
