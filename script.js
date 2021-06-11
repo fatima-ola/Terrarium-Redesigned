051d03420b0b088cc1d2f0306b9ae4247fe3c070
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-06-11 21:21:35 +0100

    fixed the drag and drop issue

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ dragElement(document.getElementById("plant14"));
 
 function dragElement(terrariumElement){
     let pos1 = 0;
-    pos2 = 0;
-    pos3 = 0;
-    pos4 = 0;
-    terrariumElement.onclick = pointerDrag;
+    let pos2 = 0;
+    let pos3 = 0;
+    let pos4 = 0;
+    terrariumElement.onpointerdown = pointerDrag;
 
     function pointerDrag(e) {
         e.preventDefault();
@@ -35,8 +35,9 @@ function dragElement(terrariumElement){
         pos3 = e.clientX;
         pos4 = e .clientY;
         console.log(pos1, pos2, pos3, pos4);
+        // terrariumElement.position = 'absolute'
         terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
-        terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+		terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
     }
 
     function stopElementDrag() {
@@ -45,3 +46,5 @@ function dragElement(terrariumElement){
     }
     
 }
+
+
